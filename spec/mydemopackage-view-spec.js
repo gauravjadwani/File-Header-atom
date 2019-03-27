baf1c14599c758f3729d7b952d19503d64e36358
Gaurav <gaurav.jadwani@hotmail.com>
2019-03-27 18:32:19 +0530

    init

---
+++ spec/mydemopackage-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MydemopackageView from '../lib/mydemopackage-view';
+
+describe('MydemopackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
---
+++ temp.js
@@ -0,0 +1,9 @@
+/*
+@Author: Ankit
+@Name: Adam
+@Email: Adam@eve.com
+@gitlab: https://gitlab.com
+@Copyright: https://gitlab.com
+@last modified by: Ankit
+@Last Modified: March 22nd 2019, 5:17:17 pm
+*/
